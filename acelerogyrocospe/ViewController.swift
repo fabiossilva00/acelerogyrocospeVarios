3065f96557cee2f486704b0b48c22cbf93c6ca20
fabiossilva00 <fabiossilva00@gmail.com>
2017-10-31 12:43:11 -0200

    Initial Commit

---
+++ acelerogyrocospe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  acelerogyrocospe
+//
+//  Created by Fabio Sousa da Silva on 31/10/2017.
+//  Copyright © 2017 Fabio Sousa da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
