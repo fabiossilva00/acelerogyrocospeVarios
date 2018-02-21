93b125d2cd5086ff720d8b8013a131e25a0d220f
fabiossilva00 <fabiossilva00@gmail.com>
2018-02-21 12:45:05 -0300

    Final Aranha

--- Transitions/TransicaoBaixoViewController.swift
+++ Transitions/TransicaoBaixoViewController.swift
@@ -4,25 +4,62 @@
 //
 //  Created by Fabio Sousa da Silva on 21/02/2018.
 //  Copyright © 2018 Fabio Sousa da Silva. All rights reserved.
+//
 //https://medium.com/ios-os-x-development/uiview-animation-in-swift-3-2b499abb58c5
-
+//https://www.raywenderlich.com/76200/basic-uiview-animation-swift-tutorial
+//
 
 import UIKit
 
 class TransicaoBaixoViewController: UIViewController {
 
     @IBOutlet weak var viewTransicao: UIView!
+    @IBOutlet weak var algumaLabel: UILabel!
+    @IBOutlet weak var imageAranha: UIImageView!
+    @IBOutlet weak var viewTeia: UIView!
+    @IBOutlet weak var animaButton: UIButton!
+    
+    let screen = UIScreen.main.bounds
     
     func viewSome() {
         
+        let yScreen = screen.width
+        let xScreen = screen.height
+        
         UIView.animate(withDuration: 2.5, animations: {
+            self.animaButton.isEnabled = false
             self.viewTransicao.isHidden = false
             self.viewTransicao.frame.origin.y -= self.viewTransicao.frame.height
-        }, completion: nil)
+        }) { _ in
+            UIView.animate(withDuration: 2.5, delay: 2.5, animations: {
+                self.viewTransicao.frame.origin.y += self.viewTransicao.frame.height
+            }) { _ in
+                UIView.animate(withDuration: 4.5, delay: 0.7, options: .allowUserInteraction, animations: {
+                    self.imageAranha.isHidden = false
+                    self.viewTeia.isHidden = false
+                    self.imageAranha.frame.origin.y += yScreen
+                    self.viewTeia.frame.origin.y += yScreen
+                    
+                }) { _ in
+                    UIView.animate(withDuration: 1.5, delay: 0.0, options: .allowUserInteraction, animations: {
+                        self.imageAranha.frame.origin.y += 65
+                        self.imageAranha.frame.origin.x -= 65
+                        
+                    }) { _ in
+                        UIView.animate(withDuration: 2.0, delay: 0.0, options: .allowUserInteraction, animations: {
+                            self.imageAranha.frame.origin.x += xScreen
+                            
+                        })
+                    }
+                }
+            }
+        }
         
-        UIView.animate(withDuration: 2.5, delay: 5, animations: {
-            self.viewTransicao.frame.origin.y += self.viewTransicao.frame.height
-        }, completion: nil)
+    }
+    
+    @objc func mudaTela() {
+        let buttonTelasID = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "buttonTelasID")
+        self.present(buttonTelasID, animated: true, completion: nil)
         
     }
     
@@ -32,9 +69,7 @@ class TransicaoBaixoViewController: UIViewController {
     }
     
     @IBAction func voltarButton(_ sender: Any) {
-        
-        let buttonTelasID = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "buttonTelasID")
-        self.present(buttonTelasID, animated: true, completion: nil)
+        mudaTela()
         
     }
     
@@ -42,13 +77,29 @@ class TransicaoBaixoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         viewTransicao.isHidden = true
+        imageAranha.isHidden = true
+        viewTeia.isHidden = true
+        self.imageAranha.isUserInteractionEnabled = true
 
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapAranha))
+        imageAranha.addGestureRecognizer(tap)
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @objc func tapAranha() {
+    
+        let alertController = UIAlertController(title: "Ai!", message: "", preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "Tchau!", style: .cancel, handler: { _ in
+            self.mudaTela()
+        })
+        alertController.addAction(okButton)
+        self.present(alertController, animated: true, completion: nil)
+        
     }
     
 
